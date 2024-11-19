5fa1a15f68166c476d587d86e11903cafbafb649
pheonix_23 <hedahdia.faycel@gmail.com>
2024-11-19 16:15:43 +0100

    init repos

---
+++ src/test/java/org/training/springsecuritysaml/SpringSecuritySamlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.springsecuritysaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
